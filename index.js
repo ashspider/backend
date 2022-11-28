12b8bd532648edd89ced9e23f3f41208fdcba4f3
Ashish <ashish.jan93@gmail.com>
2022-11-28 10:58:15 +0530

    api

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const app = require("./express");
+const cors = require('cors');
+app.use(cors({
+
+    origin: "*"
+
+}));
+
 
 const PORT = 5000;
-app.listen(PORT, ()=> {
-    console.log("server Started at port 5000");
-});
+app.listen(PORT, () => {
+    try {
+        const options = {
+            key: fs.readFileSync("/etc/letsencrypt/live/api.nftindia.foundation/privkey.pem"),
+            cert: fs.readFileSync("/etc/letsencrypt/live/api.nftindia.foundation/fullchain.pem"),
+        }
+        console.log("server Started at port 5000");
+    } catch (error) {
+        console.log("error in catch : ", error);
+    }
+})
+
